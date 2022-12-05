678b96e30b05ad05ddb650a8d6099861564cf5be
alfredowino <ogzialfed@gmail.com>
2022-12-05 18:56:17 +0300

    creating the new views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -24,7 +25,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day, self.slug])
+
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
